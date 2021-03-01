c4b85555d547eae402e96c3628dbc048c9e315a4
kaj <zekewotring@icloud.com>
2021-03-01 10:09:35 +0000

    bakery

---
+++ Cogs/Bakery.py
@@ -0,0 +1,120 @@
+import ast
+import discord
+import config
+import traceback
+import datetime
+import random
+import asyncio
+import market
+
+from discord.ext import commands, tasks
+
+class Bakery(commands.Cog):
+
+    def __init__(self, bot):
+        self.bot = bot
+    
+    @commands.command()
+    async def bakery(self, ctx):
+        user = config.get_user(ctx.author.id)
+
+        embed = discord.Embed(title="Your Bakery", color=config.MAINCOLOR, description = f"{config.stove_burning[False]} `{user['oven_count']}` Total Ovens\n{config.stove_burning[True]} `{len(user['ovens'])}` Baking Ovens\n{config.stove_burning[False]} `{user['oven_count'] - len(user['ovens'])}` Free Ovens")
+
+        for _ in range(user['oven_count']):
+            try:
+                current = user['ovens'][_]
+            except IndexError:
+                current = None
+
+            if current is None:
+                embed.add_field(name=f"{config.stove_burning[False]}", value=f"Oven is empty.\n-")
+            else:
+                s = (current['done'] - datetime.datetime.utcnow()).total_seconds()
+                if s > 0:
+                    hours, remainder = divmod(s, 3600)
+                    minutes, seconds = divmod(remainder, 60)
+                    embed.add_field(name=f"{config.stove_burning[True]}", value=f"{current['name']}\n{hours}h {minutes}m {seconds}s")
+                else:
+                    embed.add_field(name=f"{config.stove_burning[False]}", value=f"{current['name']}\nplate with `pan plate`.")
+        
+        embed.set_footer(text="pan bake <bread> | pan plate")
+
+    @commands.command()
+    async def bake(self, ctx, bread:str=None):
+        user = config.get_user(ctx.author.id)
+
+        active = 0
+        for o in user['ovens']:
+            if o is not None:
+                active += 1
+        if active >= user['oven_count']:
+            await ctx.send("<:melonpan:815857424996630548> `You have bread in all of your ovens already!")
+            return
+
+        if bread is None:
+            await ctx.send("<:melonpan:815857424996630548> `You must tell me an item you wish to bake: e.g. 'bake baguette'`")
+            return
+        
+        selected = None
+        for r in config.breads:
+            if bread.lower() in r['name'].lower():
+                selected = r
+                break
+            try:
+                index = int(bread)
+                if index == config.breads.index(r):
+                    selected = r
+                    break
+            except:
+                pass
+        if selected is None:
+            await ctx.send("<:melonpan:815857424996630548> `That bread doesn't look like it exists...`")
+        else:
+            bake_obj = {
+                'name': selected['name'],
+                'index': config.breads.index(selected),
+                'done': datetime.datetime.utcnow() + datetime.timedelta(minutes=selected['bake_time'])
+            }
+            entered = False
+            for o in user['ovens']:
+                if o is None:
+                    user['ovens'][user['ovens'].index(o)] = bake_obj
+                    entered = True
+            if not entered:
+                user['ovens'].append(bake_obj)
+            config.USERS.update_one({'id': user['id']}, {'$set': {'ovens': user['ovens']}})
+            await ctx.send(f"{config.stove_burning[True]} Your **{bake_obj['name']}** is now baking! use `pan bakery` to check on it, and `pan plate` to take it out when it's done.")
+
+    @commands.command()
+    async def plate(self, ctx):
+        user = config.get_user(ctx.author.id)
+
+        ending = ""
+        cutoff = False
+
+        for o in user['ovens']:
+            if o is not None:
+                s = (o['done'] - datetime.datetime.utcnow()).total_seconds()
+                if s < 0:
+                    if len(user['inventory']) >= 25:
+                        cutoff = True
+                        break
+                    new_bread = config.create_bread(config.breads[o['index']])
+                    user['inventory'].append(new_bread)
+                    ending += f"+ `{config.quality_levels[new_bread['quality']]}` **{new_bread['name']}**\n"
+                    user['ovens'][user['ovens'].index(o)] = None
+        
+        config.USERS.update_one({'id': user['id']}, {'$set': {'inventory': user['inventory'], 'ovens': user['ovens']}})
+        
+        embed = discord.Embed(color=config.MAINCOLOR, title="Plated Bread", description=ending)
+        if cutoff:
+            embed.description += "\n*Some ovens were not emptied because your bread storage is full. Please sell some bread.*"
+        
+
+
+
+            
+
+
+def setup(bot):
+    bot.add_cog(Bakery(bot))
--- config.py
+++ config.py
@@ -54,7 +54,7 @@ def get_cached_member(id):
 def get_prefix(id):
     server = PREFIXES.find_one({'id': id})
     if server is None:
-        server = {'id': id, 'prefix': "n!"}
+        server = {'id': id, 'prefix': "pan "}
     return server
 
 def get_user(id):
@@ -65,7 +65,8 @@ def get_user(id):
             'inventory': [],
             'money': 10,
             'baked': 0,
-            'bakery': []
+            'ovens': [],
+            'oven_count': 3
         }
         USERS.insert_one(user)
     return user
@@ -77,59 +78,69 @@ def create_bread(bread):
     }
 
 quality_levels = {1: "☆☆☆☆", 2: "★☆☆☆", 3: "★★☆☆", 4: "★★★☆", 5: "★★★★"}
+stove_burning = {True: "<:stove_burning:815877702410043432>", False: "<:stove:815875824376610837>"}
 
 breads = [
     {
         "name": "White Bread",
         "price": 13,
+        "bake_time": 35,
         "image": "https://i.imgur.com/ZAyk2Te.png",
         "description": "White bread typically refers to breads made from wheat flour from which the bran and the germ layers have been removed from the whole wheatberry as part of the flour grinding or milling process, producing a light-colored flour."
     },
     {
         "name": "Whole Wheat",
         "price": 15,
+        "bake_time": 30,
         "image": "https://i.imgur.com/ytSQ8Jb.png",
         "description": "Whole wheat bread or wholemeal bread is a type of bread made using flour that is partly or entirely milled from whole or almost-whole wheat grains, see whole-wheat flour and whole grain. It is one kind of brown bread."
     },
     {
         "name": "Toast",
         "price": 5,
+        "bake_time": 5,
         "image": "https://i.imgur.com/Aw3he0x.png",
         "description": "Toast is bread that has been browned by exposure to radiant heat. The browning is the result of a Maillard reaction, altering the flavor of the bread and making it firmer so that it is easier to spread toppings on it."
     },
     {
         "name": "Banana Bread",
         "price": 21,
+        "bake_time": 60,
         "image": "https://i.imgur.com/k2syNWa.png",
         "description": "Banana bread is a type of bread made from mashed bananas. It is often a moist, sweet, cake-like quick bread; however there are some banana bread recipes that are traditional-style raised breads."
     },
     {
         "name": "Baguette",
         "price": 25,
+        "bake_time": 30,
         "image": "https://i.imgur.com/S374hRf.png",
         "description": "A baguette is a long, thin loaf of French bread that is commonly made from basic lean dough. It is distinguishable by its length and crisp crust. A baguette has a diameter of about 5 to 6 centimetres and a usual length of about 65 cm, although a baguette can be up to 1 m long."
     },
     {
         "name": "Pita Bread",
         "price": 14,
+        "bake_time": 15,
         "image": "https://i.imgur.com/B9bc9YV.png",
         "description": "Pita or pitta, is a family of yeast-leavened round flatbreads baked from wheat flour, common in the Mediterranean, Middle East, and neighboring areas."
     },
     {
         "name": "Sourdough Bread",
-        "price": 11,
+        "price": 41,
+        "bake_time": 120,
         "image": "https://i.imgur.com/K7ZhxD4.png",
         "description": "Sourdough bread is made by the fermentation of dough using naturally occurring lactobacilli and yeast. The lactic acid produced by the lactobacilli gives it a more sour taste and improved keeping qualities."
     },
     {
         "name": "Bagel",
         "price": 2,
+        "bake_time": 35,
         "image": "https://i.imgur.com/CtxrhUn.png",
         "description": "A bagel, also historically spelled beigel, is a bread product originating in the Jewish communities of Poland. It is traditionally shaped by hand into the form of a ring from yeasted wheat dough, roughly hand-sized, that is first boiled for a short time in water and then baked."
     },
     {
         "name": "Melonpan",
         "price": 45,
+        "bake_time": 30,
         "image": "https://i.imgur.com/WWcYes4.png",
         "description": "A melonpan is a type of sweet bun from Japan, that is also popular in Taiwan and China. They are made from an enriched dough covered in a thin layer of crisp cookie dough. Their appearance resembles a melon, such as a rock melon."
     }
