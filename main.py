59783e2f9fc80134fc091b1acb4d2e4fcbf32863
kaj <zekewotring@icloud.com>
2021-03-04 04:37:39 +0000

    fix

--- main.py
+++ main.py
@@ -55,7 +55,7 @@ async def invite(ctx):
     await ctx.send(embed=discord.Embed(description="[**Invite Link**](https://discord.com/api/oauth2/authorize?client_id=815835732979220501&permissions=314433&scope=bot) 🔗", color = config.MAINCOLOR))
 
 # Cogs
-cogs = ["Eval", "Information", "Market", "Bakery", "StatCord", "Leaderboards", "Badges", "Blacklist", "BreadmojiEvent"]
+cogs = ["Eval", "Information", "Market", "Bakery", "StatCord", "Leaderboards", "Badges", "Blacklist"]
 
 # Starts all cogs
 for cog in cogs:
