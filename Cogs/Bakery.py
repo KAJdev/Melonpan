09e7fcb18b1804d254971d39253c15d58cfb08d5
kaj <zekewotring@icloud.com>
2021-03-04 00:51:25 +0000

    fix usage

--- Cogs/Bakery.py
+++ Cogs/Bakery.py
@@ -73,7 +73,7 @@ class Bakery(commands.Cog):
             return
 
         if bread is None:
-            await ctx.send("<:melonpan:815857424996630548> `You must tell me an item you wish to bake: e.g. 'bake baguette'`")
+            await ctx.send("<:melonpan:815857424996630548> `You must tell me an item you wish to bake: e.g. 'bakeall baguette'`")
             return
         
         selected = None
