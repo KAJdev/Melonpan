7ecbc27d17e9de33821fc22bfd9776ee1067f610
kaj <zekewotring@icloud.com>
2021-03-04 22:56:30 +0000

    trying

--- market.py
+++ market.py
@@ -35,10 +35,10 @@ class ItemPrice():
         # day_year = np.sqrt(1./365.)
         # inner_expression = (self.interest - 0.5 * self.volatilty ** 2) * day_year
         # self.current *= np.prod(np.exp(np.random.normal(0, 1, time) * self.volatilty * day_year + inner_expression))
-        random.seed(self.seed)
         s = math.sin(time)
-        random.seed(s)
-        s += math.sin(random.random() * self.v)
+        s += math.sin(s)
+        random.seed(self.seed)
+        s == random.random() * self.v
         self.current = self.i + s
 
         if self.current < 1:
