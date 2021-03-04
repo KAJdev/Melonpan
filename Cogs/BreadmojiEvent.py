81e7ee5efe584fc881c69194cec06ca9bc75056e
kaj <zekewotring@icloud.com>
2021-03-04 02:42:27 +0000

    breadmoji event

---
+++ Cogs/BreadmojiEvent.py
@@ -0,0 +1,50 @@
+import ast
+import discord
+import config
+import traceback
+import datetime
+import market
+
+from discord.ext import commands, tasks
+
+class BreadmojiEvent(commands.Cog):
+
+    def __init__(self, bot):
+        self.bot = bot
+        self.delete.start()
+        self.to_delete = []
+        self.allowed = ['🥯', '🥖', '🫓', '🍞', '🥐', '🥨']
+        self.channel = 816841054983946271
+        self.guild = 814958240009420830
+    
+    def cog_unload(self):
+        self.delete.cancel()
+
+    @tasks.loop(seconds=1)
+    async def delete(self):
+        if isinstance(self.channel, int):
+            g = self.bot.get_guild(self.guild)
+            if g is not None:
+                t = self.channel
+                self.channel = g.get_channel(self.channel)
+                if self.channel is None:
+                    self.channel = t
+                    return
+        
+        if len(self.to_delete) > 0:
+            def check(message):
+                return message.id in self.to_delete
+
+            await self.channel.purge(limit=100, check=check, bulk=True)
+
+            self.to_delete = []
+
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        if message.channel.id == self.channel:
+            if message.content not in self.allowed:
+                self.to_delete.append(message.id)
+
+
+def setup(bot):
+    bot.add_cog(BreadmojiEvent(bot))
