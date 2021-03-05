b6cd773668d6c312b83019536b8cfcc7ce164bce
kaj <zekewotring@icloud.com>
2021-03-05 02:56:54 +0000

    better

--- Cogs/Information.py
+++ Cogs/Information.py
@@ -25,7 +25,7 @@ class InventoryMenu(menus.ListPageSource):
             description=desc
         )
         print(entries, offset)
-        embed.set_footer(text=f"Showing {len(entries) - offset}/{len(entries)}")
+        embed.set_footer(text=f"Showing {len(entries)}/{len(self.data)}")
         return embed
 
 class Information(commands.Cog):
