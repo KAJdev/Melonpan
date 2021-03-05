70f1ef7ebcf9b3826de43ab512a4c348c3b41983
kaj <zekewotring@icloud.com>
2021-03-05 10:07:02 +0000

    fix

--- Cogs/Information.py
+++ Cogs/Information.py
@@ -26,7 +26,8 @@ class InventoryMenu(menus.ListPageSource):
             color=config.MAINCOLOR,
             description=desc
         )
-        embed.add_field(name="<:BreadStaff:815484321590804491> Storage Expansion", value=f"`pan expand`\nCost: `{int((max/config.expand_amount) * config.expand_cost)}` <:BreadCoin:815842873937100800>")
+        e_cost = int((max/config.expand_amount) * config.expand_cost)
+        embed.add_field(name="<:BreadStaff:815484321590804491> Storage Expansion", value=f"`pan expand`\nCost: `{e_cost}` <:BreadCoin:815842873937100800>")
         embed.set_footer(text=f"Showing {menu.current_page + 1}/{menu._source.get_max_pages()} | Storage Capacity: {len(self.og)}/{self.max}")
         return embed
 
