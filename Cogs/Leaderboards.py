d548efa12698e564bf63aefda3b887edf0b8738f
kaj <zekewotring@icloud.com>
2021-03-15 02:43:54 +0000

    pan top format change

--- Cogs/Leaderboards.py
+++ Cogs/Leaderboards.py
@@ -38,7 +38,7 @@ class Leaderboards(commands.Cog):
                 user['user_object'] = found
                 self.user_cache[user['id']] = found
             # x.add_row([f"#{amount}", user['user_object'].name, f"{user['money']} BreadCoin"])
-            desc += f"`#{amount}` • {user['money']} <:BreadCoin:815842873937100800> • **{user['user_object']}**{' ' if len(user.get('badges', [])) > 0 else ''}{''.join(config.badges[x]['emoji'] for x in user.get('badges', []))}\n"
+            desc += f"`#{amount}` • **{user['money']:,}** <:BreadCoin:815842873937100800> • {user['user_object'].name}{' ' if len(user.get('badges', [])) > 0 else ''}{''.join(config.badges[x]['emoji'] for x in user.get('badges', []))}\n"
             amount += 1
 
         # x.header = False
