06901c6f066a38135661d21d995bb198c877b9a3
KAJ7 <zekewotring@gmail.com>
2021-04-04 23:32:53 -0800

    imports

--- Cogs/Mongo.py
+++ Cogs/Mongo.py
@@ -9,6 +9,10 @@ import random
 from discord.ext import commands, tasks
 from discord_slash import cog_ext, SlashContext
 from discord_slash.utils.manage_commands import create_option, create_choice
+import os
+import uuid
+from random_word import RandomWords
+import string
 
 class Server():
     def __init__(self, server):
